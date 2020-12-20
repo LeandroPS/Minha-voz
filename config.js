a4cc17ffdc770b0ef21e2c77f697fc409df47dfd
LeandroPS <leandro.pires.souza@gmail.com>
2020-12-19 21:53:17 -0300

    Add config data

---
+++ config.js
@@ -0,0 +1,5 @@
+export default {
+  APIUrl:
+    "https://api.us-south.text-to-speech.watson.cloud.ibm.com/instances/8813a0cf-4927-4328-aaa7-aa77e48eb17e",
+  APIKey: "rNJWloVM2-duAZ0iz9GyCdlKvGNTJY7DSkRSzXAfH6ZQ",
+};
