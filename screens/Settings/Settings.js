016b65079585c227ea6333fe85ca971317fa9bb8
LeandroPS <leandro.pires.souza@gmail.com>
2020-12-25 19:39:07 -0300

    [Settings] Create dummy settings page

---
+++ screens/Settings/Settings.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text } from "react-native";
+
+import Container from "../../components/Container";
+
+const Settings = () => {
+  return (
+    <Container>
+      <Text>Settings</Text>
+    </Container>
+  );
+};
+
+export default Settings;
