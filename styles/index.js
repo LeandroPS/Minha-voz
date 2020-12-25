22b5e4e87341bae6c2eb22b60c9401376bbf0c8d
LeandroPS <leandro.pires.souza@gmail.com>
2020-12-25 11:24:07 -0300

    [styles] Add font-size styles

--- styles/index.js
+++ styles/index.js
@@ -5,4 +5,8 @@ export default {
   "spacing-4": "20px",
   "spacing-5": "32px",
   "spacing-6": "52px",
+  "font-sizing-1": "8px",
+  "font-sizing-2": "16px",
+  "font-sizing-3": "24px",
+  "font-sizing-4": "32px",
 };
