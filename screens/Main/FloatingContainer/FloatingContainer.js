6c945d5ba51caf2f43972dc6fd61bc0bc6083190
LeandroPS <leandro.pires.souza@gmail.com>
2020-12-31 03:13:27 -0300

    [FloatingContainer] Set fixed positions for children items

--- screens/Main/FloatingContainer/FloatingContainer.js
+++ screens/Main/FloatingContainer/FloatingContainer.js
@@ -1,10 +1,35 @@
+import React from "react";
 import styled from "styled-components/native";
 import styles from "../../../styles";
 
 const FloatingContainer = styled.View`
-  padding: 0 ${styles["spacing-3"]};
+  padding: 0 ${styles["spacing-5"]};
   position: absolute;
   bottom: ${styles["spacing-2"]};
+  flex: 1;
+  align-items: center;
+  flex-direction: row;
+  width: 100%;
 `;
 
-export default FloatingContainer;
+const LeftView = styled.View`
+  flex: 1;
+`;
+
+const CenterView = styled.View`
+  flex: 2;
+`;
+
+const RightView = styled.View`
+  flex: 1;
+`;
+
+const FloatingContainerWrapper = ({ left, center, right }) => (
+  <FloatingContainer>
+    <LeftView>{left}</LeftView>
+    <CenterView>{center}</CenterView>
+    <RightView>{right}</RightView>
+  </FloatingContainer>
+);
+
+export default FloatingContainerWrapper;
