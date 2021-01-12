f2e6346d12eaf82786e8c90195220d91f6b31623
LeandroPS <leandro.pires.souza@gmail.com>
2021-01-11 22:16:10 -0300

    [Heading] Create Heading component

---
+++ screens/Main/Heading/Heading.js
@@ -0,0 +1,11 @@
+import styled from "styled-components/native";
+import styles from "../../../styles";
+
+const Heading = styled.Text`
+  color: ${styles["color-text"]};
+  font-weight: bold;
+  text-transform: uppercase;
+  margin-bottom: ${styles["spacing-2"]};
+`;
+
+export default Heading;
