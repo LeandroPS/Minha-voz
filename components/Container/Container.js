a8f275a52d09a9132a402d595031f673ba4c09dc
LeandroPS <leandro.pires.souza@gmail.com>
2021-01-15 00:48:01 -0300

    [Container] Fix ScrollView

--- components/Container/Container.js
+++ components/Container/Container.js
@@ -1,27 +1,35 @@
 import React from "react";
-import { Platform } from "react-native";
+import { Platform, View } from "react-native";
 import { useHeaderHeight } from "@react-navigation/stack";
-import {
-  SafeAreaView,
-  useSafeAreaInsets,
-} from "react-native-safe-area-context";
+import { SafeAreaView } from "react-native-safe-area-context";
 import styled from "styled-components/native";
+import styles from "../../styles";
 
 const KeyboardAvoidingView = styled.KeyboardAvoidingView`
   flex: 1;
+  background-color: ${styles["color-background"]};
 `;
 
 const StyledSafeAreaView = styled(SafeAreaView)`
   flex: 1;
 `;
 
-const Container = ({ children }) => (
+const ScrollView = styled.ScrollView`
+  flex: 1;
+  padding: 0 ${styles["spacing-4"]};
+`;
+
+const Container = ({ children, afterScrollView, ...props }) => (
   <KeyboardAvoidingView
     keyboardVerticalOffset={useHeaderHeight()}
     behavior={Platform.OS == "ios" ? "padding" : "height"}
+    {...props}
   >
     <StyledSafeAreaView edges={["top", "bottom"]} bottom={20}>
-      {children}
+      <View style={{ flex: 1 }}>
+        <ScrollView>{children}</ScrollView>
+      </View>
+      {afterScrollView}
     </StyledSafeAreaView>
   </KeyboardAvoidingView>
 );
