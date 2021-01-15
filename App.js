e9dc97690a907060161ca02e47b75e23b5f15303
LeandroPS <leandro.pires.souza@gmail.com>
2021-01-15 00:49:48 -0300

    [App] use Favorites provider

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { encode, decode } from "base-64";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import FavoritesProvider from "./hooks/Favorites";
 import HeaderLogo from "./components/HeaderLogo";
 import Main from "./screens/Main";
 import Settings from "./screens/Settings";
@@ -20,20 +21,28 @@ const { Navigator, Screen } = createStackNavigator();
 
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <NavigationContainer>
-        <Navigator>
-          <Screen
-            name="Main"
-            component={Main}
-            options={{
-              headerLeft: () => <HeaderLogo />,
-              headerTitle: null,
-            }}
-          />
-          <Screen name="Settings" component={Settings} />
-        </Navigator>
-      </NavigationContainer>
-    </SafeAreaProvider>
+    <FavoritesProvider>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <Navigator>
+            <Screen
+              name="Main"
+              component={Main}
+              options={{
+                headerLeft: () => <HeaderLogo />,
+                headerTitle: null,
+                headerStyle: {
+                  elevation: 0,
+                  shadowOpacity: 0,
+                  borderBottomWidth: 1,
+                },
+              }}
+            />
+            <Screen name="Settings" component={Settings} />
+          </Navigator>
+        </NavigationContainer>
+      </SafeAreaProvider>
+    </FavoritesProvider>
   );
 }
