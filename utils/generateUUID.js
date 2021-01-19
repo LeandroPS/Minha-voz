7611a233030b2a8ddcde373e1128625ca77126fe
LeandroPS <leandro.pires.souza@gmail.com>
2021-01-18 23:38:29 -0300

    [generateUUID] Fix util

--- utils/generateUUID.js
+++ utils/generateUUID.js
@@ -1,6 +1,6 @@
 const generateUUID = () => {
-  const d = new Date().getTime();
-  const d2 = (performance && performance.now && performance.now() * 1000) || 0;
+  let d = new Date().getTime();
+  let d2 = (performance && performance.now && performance.now() * 1000) || 0;
   return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, (c) => {
     let r = Math.random() * 16;
     if (d > 0) {
