c49bd7727a8e76e739a9e04e5e3f95681270ea7a
LeandroPS <leandro.pires.souza@gmail.com>
2021-01-18 23:09:23 -0300

    [FloatingContainer] Add gradient to background

--- screens/Main/FloatingContainer/FloatingContainer.js
+++ screens/Main/FloatingContainer/FloatingContainer.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { LinearGradient } from "expo-linear-gradient";
 import styled from "styled-components/native";
 import styles from "../../../styles";
 
-const FloatingContainer = styled.View`
+const FloatingContainer = styled(LinearGradient)`
   padding: 0 ${styles["spacing-5"]};
+  padding-top: ${styles["spacing-4"]};
   position: absolute;
+  left: 0;
   bottom: ${styles["spacing-2"]};
   flex: 1;
   align-items: center;
@@ -25,7 +28,10 @@ const RightView = styled.View`
 `;
 
 const FloatingContainerWrapper = ({ left, center, right }) => (
-  <FloatingContainer>
+  <FloatingContainer
+    colors={["rgba(255, 255, 255, 0)", styles["color-background"]]}
+    locations={[0, 0.5]}
+  >
     <LeftView>{left}</LeftView>
     <CenterView>{center}</CenterView>
     <RightView>{right}</RightView>
