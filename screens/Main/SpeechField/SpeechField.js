b9b552f73fc1acd2ee8ef578ab0fd386ff486cc4
LeandroPS <leandro.pires.souza@gmail.com>
2021-02-11 22:07:27 -0300

    [SpeechField] Improve styles

--- screens/Main/SpeechField/SpeechField.js
+++ screens/Main/SpeechField/SpeechField.js
@@ -2,15 +2,20 @@ import styled from "styled-components/native";
 import styles from "../../../styles";
 
 const SpeechField = styled.TextInput`
-  padding: 0 ${styles["spacing-4"]} ${styles["spacing-4"]};
-  margin-bottom: 20px;
-  font-size: ${styles["font-sizing-4"]};
-  flex: 1;
+  padding: ${styles["spacing-4"]} 0;
+  font-size: ${styles["font-sizing-6"]};
+
+  align-content: flex-start;
+  color: ${styles["color-text"]};
+  min-height: 300px;
+  margin-bottom: ${styles["spacing-4"]};
 `;
 
 SpeechField.defaultProps = {
+  textAlignVertical: "top",
   multiline: true,
   placeholder: "Digite seu texto aqui e depois clique no botão 'play'",
+  placeholderTextColor: styles["color-placeholder"],
 };
 
 export default SpeechField;
