0070990f058b8e97376ce8c67cd4d06be193f7a7
LeandroPS <leandro.pires.souza@gmail.com>
2021-02-11 22:05:35 -0300

    [playBase64URIAudio] Improve Android compatibility

--- utils/playBase64URIAudio.js
+++ utils/playBase64URIAudio.js
@@ -1,10 +1,25 @@
 import { Audio } from "expo-av";
+import * as FileSystem from "expo-file-system";
 
-const playBase64URIAudio = async (base64URI) => {
-  const { sound } = await Audio.Sound.createAsync({
-    uri: base64URI,
-  });
-  await sound.playAsync();
+const playBase64URIAudio = async (base64String) => {
+  try {
+    const uri = `${FileSystem.cacheDirectory}/bliu.mp3`;
+
+    await FileSystem.writeAsStringAsync(uri, base64String.split("base64,")[1], {
+      encoding: FileSystem.EncodingType.Base64,
+    });
+
+    await Audio.setAudioModeAsync({
+      playsInSilentModeIOS: true,
+    });
+
+    const { sound } = await Audio.Sound.createAsync({
+      uri,
+    });
+    await sound.playAsync();
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 export default playBase64URIAudio;
