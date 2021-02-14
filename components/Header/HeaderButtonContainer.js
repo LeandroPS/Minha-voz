313e1942c34fec0fe7df4d44ad397603529067e2
LeandroPS <leandro.pires.souza@gmail.com>
2021-02-14 15:33:37 -0300

    [HeaderButtonContainer] Create component

---
+++ components/Header/HeaderButtonContainer.js
@@ -0,0 +1,11 @@
+import styled from "styled-components/native";
+import styles from "../../styles";
+
+const HeaderButtonContainer = styled.View`
+  padding: 0 ${styles["spacing-4"]};
+  flex: 1;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export default HeaderButtonContainer;
