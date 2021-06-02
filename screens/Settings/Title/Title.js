49aa931d827d3c8d70a329f5f2ac6bbff6cca948
LeandroPS <leandro.pires.souza@gmail.com>
2021-06-02 15:20:55 -0300

    [Title] Create component

---
+++ screens/Settings/Title/Title.js
@@ -0,0 +1,11 @@
+import styles from "../../../styles";
+import styled from "styled-components/native";
+
+const Title = styled.Text`
+  color: ${styles["color-text"]};
+  font-size: ${styles["font-sizing-7"]};
+  font-weight: 700;
+  margin: ${styles["spacing-3"]} 0;
+`;
+
+export default Title;
