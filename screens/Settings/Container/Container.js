677ca59f8cc153abbe4c76f20521c13c8ff518e1
LeandroPS <leandro.pires.souza@gmail.com>
2021-06-02 15:23:28 -0300

    [Settings Container] Create component

---
+++ screens/Settings/Container/Container.js
@@ -0,0 +1,10 @@
+import styles from "../../../styles";
+import styled from "styled-components/native";
+
+const Container = styled.View`
+  background-color: white;
+  flex: 1;
+  padding: ${styles["spacing-2"]} ${styles["spacing-4"]};
+`;
+
+export default Container;
