f319e8c645eefcc08f885358c4ab0cbe91e7de13
LeandroPS <leandro.pires.souza@gmail.com>
2021-06-02 15:24:34 -0300

    [Settings] Add Speech rate settings

--- screens/Settings/Settings.js
+++ screens/Settings/Settings.js
@@ -1,12 +1,32 @@
 import React from "react";
-import { Text } from "react-native";
-
-import Container from "../../components/Container";
+import { useSettings } from "../../hooks/Settings";
+import Slider from "@react-native-community/slider";
+import Container from "./Container";
+import Section from "./Section";
+import Title from "./Title";
 
 const Settings = () => {
+  const { settings, setSettings } = useSettings();
+  const { speed } = settings;
+
+  const setSpeed = (speedValue) => {
+    setSettings({
+      ...settings,
+      speed: speedValue,
+    });
+  };
+
   return (
     <Container>
-      <Text>Settings</Text>
+      <Title>Configurações</Title>
+      <Section title="Velocidade de fala">
+        <Slider
+          onSlidingComplete={setSpeed}
+          value={speed}
+          minimumValue={0}
+          maximumValue={2}
+        />
+      </Section>
     </Container>
   );
 };
