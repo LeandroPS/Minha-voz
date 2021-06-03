0c35ac11389c7cd7e7c0051ddeddc50d5bd218a7
LeandroPS <leandro.pires.souza@gmail.com>
2021-06-03 15:10:22 -0300

    [FloatingContainer] Refactor component

--- screens/Main/FloatingContainer/FloatingContainer.js
+++ screens/Main/FloatingContainer/FloatingContainer.js
@@ -5,7 +5,7 @@ import styled from "styled-components/native";
 import useKeyboard from "../../../hooks/useKeyboard";
 import styles from "../../../styles";
 
-const FloatingContainer = styled(LinearGradient)`
+const Container = styled(LinearGradient)`
   padding: 0 ${styles["spacing-5"]};
   padding-top: ${styles["spacing-4"]};
   padding-bottom: ${styles["spacing-3"]};
@@ -13,39 +13,48 @@ const FloatingContainer = styled(LinearGradient)`
   left: 0;
   bottom: ${(props) => props.bottom};
   flex: 1;
-  align-items: center;
-  flex-direction: row;
+
+  flex-direction: column;
   width: 100%;
 `;
 
-const LeftView = styled.View`
+const Row = styled.View`
   flex: 1;
+  align-items: center;
+  justify-content: center;
+  flex-direction: row;
+  margin-top: ${styles["spacing-2"]};
 `;
 
-const CenterView = styled.View`
+const SingleSpaceCell = styled.View`
+  flex: 1;
+  justify-content: center;
+  height: 100%;
+`;
+
+const DoubleSpaceCell = styled(SingleSpaceCell)`
   flex: 2;
 `;
 
-const RightView = styled.View`
-  flex: 1;
-`;
-
-const FloatingContainerWrapper = ({ left, center, right }) => {
+const FloatingContainer = ({ top, left, center, right }) => {
   const paddedInset = Math.min(useSafeAreaInsets().bottom, 20);
   const bottomPadding = `${paddedInset}px`;
   const isKeyboardVisible = useKeyboard();
 
   return (
-    <FloatingContainer
+    <Container
       colors={["rgba(255, 255, 255, 0)", styles["color-background"]]}
       locations={[0, 0.7]}
       bottom={isKeyboardVisible ? 0 : bottomPadding}
     >
-      <LeftView>{left}</LeftView>
-      <CenterView>{center}</CenterView>
-      <RightView>{right}</RightView>
-    </FloatingContainer>
+      {top && <Row>{top}</Row>}
+      <Row>
+        <SingleSpaceCell>{left}</SingleSpaceCell>
+        <DoubleSpaceCell>{center}</DoubleSpaceCell>
+        <SingleSpaceCell>{right}</SingleSpaceCell>
+      </Row>
+    </Container>
   );
 };
 
-export default FloatingContainerWrapper;
+export default FloatingContainer;
