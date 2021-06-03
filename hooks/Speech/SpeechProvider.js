bb254cfb7604bc1c019964a60ebd85ca41c261b0
LeandroPS <leandro.pires.souza@gmail.com>
2021-06-03 15:09:48 -0300

    [SpeechProvider] Expose error value

--- hooks/Speech/SpeechProvider.js
+++ hooks/Speech/SpeechProvider.js
@@ -1,4 +1,5 @@
 import React, { useState, createContext } from "react";
+import convertBlobToBase64 from "../../utils/convertBlobToBase64";
 import fetchAudio from "../../services/fetchAudio";
 import playBase64URIAudio from "../../utils/playBase64URIAudio";
 import * as Analytics from "expo-firebase-analytics";
@@ -7,26 +8,30 @@ const SpeechContext = createContext();
 
 const SpeechProvider = ({ children }) => {
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   const speak = async (text) => {
     if (text === "") return;
 
     try {
       setLoading(true);
-      const audio = await fetchAudio(text);
+      const audioBlob = await fetchAudio(text);
+      const audio = convertBlobToBase64(audioBlob);
       setLoading(false);
+      setError(null);
       playBase64URIAudio(audio);
 
       await Analytics.logEvent("spokeText", {
         length: text.length,
       });
     } catch (error) {
-      console.error(error.message);
+      setLoading(false);
+      setError(error.message);
     }
   };
 
   return (
-    <SpeechContext.Provider value={{ loading, speak }}>
+    <SpeechContext.Provider value={{ loading, speak, error }}>
       {children}
     </SpeechContext.Provider>
   );
